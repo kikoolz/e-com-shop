54acefd5b19d3a780c8be9c941205fc44f5be58a
kikoolz <kenlubs45@gmail.com>
2025-12-27 17:13:19 +0300

    Added a test console.log in the about page

--- app/(marketing)/about/page.tsx
+++ app/(marketing)/about/page.tsx
@@ -1,4 +1,5 @@
 const AboutPage = () => {
+  console.log("Rendering About Page");
   return (
     <div className="mx-auto max-w-4xl px-4 py-12 sm:px-6 lg:px-8">
       <h1 className="mb-8 text-3xl font-bold text-zinc-900 dark:text-zinc-100 sm:text-4xl">
